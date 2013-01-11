068e96824bfc91fd813dd154ce9521e34878fd0b
zachkinstner <zach@aestheticinteractive.com>
2013-01-11 15:13:40 -0500

    Added FinishWithout func to the Transaction interface.

--- Solution/Weaver/Interfaces/IWeaverTransaction.cs
+++ Solution/Weaver/Interfaces/IWeaverTransaction.cs
@@ -11,6 +11,7 @@
 		void AddQuery(IWeaverQuery pQuery);
 		string GetNextVarName();
 		void Finish(WeaverTransaction.ConclusionType pConclusion, IWeaverVarAlias pFinalOutput=null);
+		void FinishWithoutStartStop(IWeaverVarAlias pFinalOutput=null);
 		
 	}
 
