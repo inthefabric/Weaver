f595ba2f621f4ca3bb88a444425dfa45f6c8131b
zachkinstner <zach@aestheticinteractive.com>
2013-02-21 16:26:18 -0500

    Refactored Id property into separate interface. All 230 tests pass.

--- Solution/Weaver/Interfaces/IWeaverItemWithId.cs
+++ Solution/Weaver/Interfaces/IWeaverItemWithId.cs
@@ -1,11 +1,9 @@
 ﻿namespace Weaver.Interfaces {
 
 	/*================================================================================================*/
-	public interface IWeaverNode : IWeaverItemWithId {
+	public interface IWeaverItemWithId : IWeaverItemIndexable {
 
-		bool IsFromNode { get; set; }
-		bool ExpectOneNode { get; set; }
-		bool IsRoot { get; }
+		long Id { get; set; }
 
 	}
 
