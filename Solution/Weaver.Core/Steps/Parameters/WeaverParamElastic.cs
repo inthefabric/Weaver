81dcce70bbb3c45bd68de2ba8fc07bfa6b72c03c
Zach Kinstner <zach@aestheticinteractive.com>
2013-06-13 17:18:25 -0400

    Added new ElasticIndex tests.
    All 245 tests pass. 100% coverage.

--- Solution/Weaver.Core/Steps/Parameters/WeaverParamElastic.cs
+++ Solution/Weaver.Core/Steps/Parameters/WeaverParamElastic.cs
@@ -27,25 +27,35 @@ namespace Weaver.Core.Steps.Parameters {
 
 		internal static IDictionary<WeaverParamElasticOp, string> OpMap;
 
+		private const string Compare = "com.tinkerpop.blueprints.Query.Compare.";
+		private const string Text = "com.thinkaurelius.titan.core.attribute.Text.";
+
+		public const string EqualToScript = Compare+"EQUAL";
+		public const string NotEqualToScript = Compare+"NOT_EQUAL";
+		public const string GreaterThanScript = Compare+"GREATER_THAN";
+		public const string GreaterThanOrEqualToScript = Compare+"GREATER_THAN_EQUAL";
+		public const string LessThanScript = Compare+"LESS_THAN";
+		public const string LessThanOrEqualToScript = Compare+"LESS_THAN_EQUAL";
+
+		public const string ContainsScript = Text+"CONTAINS";
+		public const string PrefixScript = Text+"PREFIX";
+
 
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		/*--------------------------------------------------------------------------------------------*/
 		public static string GetOperationScript(WeaverParamElasticOp pOperation) {
 			if ( OpMap == null ) {
-				const string comp = "com.tinkerpop.blueprints.Query.Compare.";
-				const string text = "com.thinkaurelius.titan.core.attribute.Text.";
-
 				OpMap = new Dictionary<WeaverParamElasticOp, string>();
 
-				OpMap.Add(WeaverParamElasticOp.EqualTo, comp+"EQUAL");
-				OpMap.Add(WeaverParamElasticOp.NotEqualTo, comp+"NOT_EQUAL");
-				OpMap.Add(WeaverParamElasticOp.GreaterThan, comp+"GREATER_THAN");
-				OpMap.Add(WeaverParamElasticOp.GreaterThanOrEqualTo, comp+"GREATER_THAN_EQUAL");
-				OpMap.Add(WeaverParamElasticOp.LessThan, comp+"LESS_THAN");
-				OpMap.Add(WeaverParamElasticOp.LessThanOrEqualTo, comp+"LESS_THAN_EQUAL");
+				OpMap.Add(WeaverParamElasticOp.EqualTo, EqualToScript);
+				OpMap.Add(WeaverParamElasticOp.NotEqualTo, NotEqualToScript);
+				OpMap.Add(WeaverParamElasticOp.GreaterThan, GreaterThanScript);
+				OpMap.Add(WeaverParamElasticOp.GreaterThanOrEqualTo, GreaterThanOrEqualToScript);
+				OpMap.Add(WeaverParamElasticOp.LessThan, LessThanScript);
+				OpMap.Add(WeaverParamElasticOp.LessThanOrEqualTo, LessThanOrEqualToScript);
 
-				OpMap.Add(WeaverParamElasticOp.Contains, text+"CONTAINS");
-				OpMap.Add(WeaverParamElasticOp.Prefix, text+"PREFIX");
+				OpMap.Add(WeaverParamElasticOp.Contains, ContainsScript);
+				OpMap.Add(WeaverParamElasticOp.Prefix, PrefixScript);
 			}
 
 			return OpMap[pOperation];
@@ -55,7 +65,6 @@ namespace Weaver.Core.Steps.Parameters {
 
 
 	/*================================================================================================*/
-	//TEST: Core.Steps.Parameters.WeaverParamElastic
 	public class WeaverParamElastic<T> : WeaverParam<T>, IWeaverParamElastic<T> 
 																			where T : IWeaverElement {
 
--- Solution/Weaver.Core/Steps/WeaverStepElasticIndex.cs
+++ Solution/Weaver.Core/Steps/WeaverStepElasticIndex.cs
@@ -1,12 +1,12 @@
 ﻿using System.Text;
 using Weaver.Core.Elements;
+using Weaver.Core.Exceptions;
 using Weaver.Core.Query;
 using Weaver.Core.Steps.Parameters;
 
 namespace Weaver.Core.Steps {
 	
 	/*================================================================================================*/
-	//TEST: Core.Steps.WeaverStepElasticIndex
 	public class WeaverStepElasticIndex<T> : WeaverStep where T : IWeaverElement {
 
 		private readonly IWeaverParamElastic<T>[] vParams;
@@ -20,11 +20,16 @@ namespace Weaver.Core.Steps {
 
 		/*--------------------------------------------------------------------------------------------*/
 		public override string BuildParameterizedString() {
+			if ( vParams.Length == 0 ) {
+				throw new WeaverStepException(this, "ElasticIndex must have at least one parameter.");
+			}
+
 			var sb = new StringBuilder();
 
-			foreach ( WeaverParamElastic<T> pe in vParams ) {
-				string propName = WrapException(() => Path.Config.GetPropertyDbName(pe.Property));
-				var valParam = Path.Query.AddParam(new WeaverQueryVal(pe.Value));
+			foreach ( IWeaverParamElastic<T> pe in vParams ) {
+				var prop = pe.Property;
+				string propName = WrapException(() => Path.Config.GetPropertyDbName(prop));
+				string valParam = Path.Query.AddParam(new WeaverQueryVal(pe.Value));
 				
 				sb.Append(
 					(sb.Length > 0 ? "." : "")+
