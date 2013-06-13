a77d6bc03d7e7b59d6f7a8fbc97479b64e5851b3
Zach Kinstner <zach@aestheticinteractive.com>
2013-06-13 17:20:22 -0400

    Updated to version 0.3.0.

--- Solution/Weaver.Core/Properties/AssemblyInfo.cs
+++ Solution/Weaver.Core/Properties/AssemblyInfo.cs
@@ -1,4 +1,9 @@
 ﻿using System.Reflection;
+using System.Resources;
 
 [assembly: AssemblyTitle("Weaver.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for building strongly-typed Gremlin queries.")]
+[assembly: AssemblyCompanyAttribute("Aesthetic Interactive")]
+[assembly: AssemblyProductAttribute("")]
+[assembly: AssemblyVersionAttribute("0.3.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
