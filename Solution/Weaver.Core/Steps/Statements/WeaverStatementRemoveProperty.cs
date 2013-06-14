5e52653a9be8f40cfbb282eee3a79686ee2e38c8
Zach Kinstner <zach@aestheticinteractive.com>
2013-06-13 21:13:28 -0400

    Added RemoveProperty statement.
    All 247 tests pass, 100% coverage.

---
+++ Solution/Weaver.Core/Steps/Statements/WeaverStatementRemoveProperty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+using Weaver.Core.Elements;
+using Weaver.Core.Path;
+
+namespace Weaver.Core.Steps.Statements {
+
+	/*================================================================================================*/
+	public class WeaverStatementRemoveProperty<T> : WeaverStatement<T> where T : IWeaverElement {
+
+		private readonly Expression<Func<T, object>> vProp;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public WeaverStatementRemoveProperty(Expression<Func<T, object>> pProperty) {
+			vProp = pProperty;
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public override string BuildParameterizedString(IWeaverPath pPath) {
+			var propName = WrapException(() => pPath.Config.GetPropertyDbName(vProp));
+			return "it.removeProperty('"+propName+"')";
+		}
+
+	}
+
+}
