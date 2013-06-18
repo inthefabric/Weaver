99f57d231bd5b0d45013d1ce641e85ddce14cdde
Zach Kinstner <zach@aestheticinteractive.com>
2013-06-17 22:26:42 -0400

    Fixed interface inheritance issue.

--- Solution/Weaver.Core/Elements/IWeaverElement.cs
+++ Solution/Weaver.Core/Elements/IWeaverElement.cs
@@ -1,9 +1,9 @@
-﻿using Weaver.Core.Path;
+﻿using Weaver.Core.Pipe;
 
 namespace Weaver.Core.Elements {
 
 	/*================================================================================================*/
-	public interface IWeaverElement : IWeaverPathItem {
+	public interface IWeaverElement : IWeaverPathPipe {
 
 		string Id { get; set; }
 
