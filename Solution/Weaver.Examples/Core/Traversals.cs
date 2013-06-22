75c47318cf465848d241021d818f591c81be42ed
Zach Kinstner <zach@aestheticinteractive.com>
2013-06-22 07:16:01 -0400

    Implemented a traversal example and test.
    Discovered a bug with edge-property-name uniqueness checks.

---
+++ Solution/Weaver.Examples/Core/Traversals.cs
@@ -0,0 +1,38 @@
+﻿using Weaver.Core.Elements;
+using Weaver.Examples.Core.EdgeTypes;
+using Weaver.Examples.Core.Vertices;
+using Weaver.Core.Query;
+using Weaver.Core.Graph;
+using Weaver.Core.Pipe;
+
+namespace Weaver.Examples.Core {
+
+	/*================================================================================================*/
+	public static class Traversals {
+	
+		//Demonstrating some traversals shown here:
+		//https://github.com/thinkaurelius/titan/wiki/Getting-Started
+		
+		//See unit tests in Weaver.Test.WeavExamples.TTraversal
+		
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public static IWeaverQuery GetCharacterByName<T>(string pName, string pVarName,
+										out IWeaverVarAlias<T> pCharVar) where T : Character, new() {
+			IWeaverQuery q = Weave.Inst.Graph
+				.V.ExactIndex<T>(x => x.Name, pName).Next()
+				.ToQuery();
+			
+			return WeaverQuery.StoreResultAsVar(pVarName, q, out pCharVar);
+		}
+		
+		/*--------------------------------------------------------------------------------------------*/
+		public static IWeaverQuery GetSaturn(out IWeaverVarAlias<Titan> pSaturnVar) {
+			//saturn = g.V('name','saturn').next()
+			return GetCharacterByName<Titan>("saturn", "saturn", out pSaturnVar);
+		}
+				
+	}
+
+}
---
+++ Solution/Weaver.Test/WeavExamples/TTraversals.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Weaver.Core;
+using Weaver.Core.Graph;
+using Weaver.Core.Pipe;
+using Weaver.Core.Query;
+using Weaver.Core.Steps;
+using Weaver.Test.Utils;
+using Weaver.Examples.Core;
+using System.Collections.Generic;
+
+namespace Weaver.Test.WeavExamples {
+
+	/*================================================================================================*/
+	[TestFixture]
+	public class TTraversals : WeaverTestBase {
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		public void GetSaturn() {
+			IWeaverVarAlias<Weaver.Examples.Core.Vertices.Titan> v;
+			IWeaverQuery q = Traversals.GetSaturn(out v);
+			
+			string expectScript = "saturn=g.V('name',_P0).next();";
+			Assert.AreEqual(expectScript, q.Script, "Incorrect script.");
+			
+			var expectParams = new Dictionary<string, IWeaverQueryVal>();
+			expectParams.Add("_P0", new WeaverQueryVal("saturn"));
+			WeaverTestUtil.CheckQueryParamsOriginalVal(q, expectParams);
+		}
+
+	}
+
+}
