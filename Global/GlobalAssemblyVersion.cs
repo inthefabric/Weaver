9b01f26174c65038a29e723feadd99f14d6f962b
Zach Kinstner <zach@aestheticinteractive.com>
2013-07-08 16:01:39 -0400

    Updated to 0.5.2.

--- Global/GlobalAssemblyVersion.cs
+++ Global/GlobalAssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyInformationalVersion("0.5.1.a9d895b")]
+[assembly: AssemblyVersion("0.5.2.0")]
+[assembly: AssemblyInformationalVersion("0.5.2.aa62eb0")]
